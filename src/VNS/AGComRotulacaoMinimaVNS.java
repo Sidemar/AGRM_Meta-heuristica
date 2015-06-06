ba37bedf9ee09eef3f6794c22f9926cbb439a3eb
Sidemar <sidemar.r7@gmail.com>
2015-06-06 16:44:40 -0300

    Aprimoramento do método guloso

--- src/VNS/AGComRotulacaoMinimaVNS.java
+++ src/VNS/AGComRotulacaoMinimaVNS.java
@@ -2,53 +2,35 @@ package VNS;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 
 public class AGComRotulacaoMinimaVNS {
     public static void main(String[] args) throws IOException {
-        ArrayList<Aresta> arestas = new ArrayList();
-        ArrayList<Aresta> arestasFinais = new ArrayList();
+        ArrayList<Aresta> arvoreGeradora = new ArrayList();
         ArrayList<Cor> cores = new ArrayList();
-        Arvore arvore = new Arvore();
+        ArrayList<Cor> coresUsadas = new ArrayList();
         
         InOut inout = new InOut("/home/sidemar/NetBeansProjects/AGRM/src/Instancias/500_124750_500.txt");
         long tempo = System.currentTimeMillis();
         
-        arestas = inout.arestas;
-        
-        cores = arvore.getCoresUsadas(arestas);
-        
-        // Como estou usando sempre as cores que mais estão presente no grafo
-        // como critério de criação da árvore geradora então estou 
-        // usando o MergeSort para ordenar essas cores
-        Collections.sort(cores);  
-        
-        for (Cor c : cores) {
-            for (Aresta a : arestas) {
-                if(a.getValor() == c.getCor()) {
-                    arestasFinais.add(0, a);
-                }
-            }
-        }
-        
-        // Complexidade theta(E), onde E é o número de arestas 
-        Collections.reverse(arestasFinais);
-        
-        arestasFinais = arvore.gerarArvore(arestasFinais, inout.N);
+        Grafo grafo = new Grafo(inout.arestas, inout.N);
+        cores = grafo.getCoresUsadas(grafo.getArestas());
+        arvoreGeradora = grafo.gerarArvore(cores);
         
         tempo = System.currentTimeMillis() - tempo;
         
-        if(arestasFinais != null) {
-            for (Aresta a : arestasFinais) {
+        coresUsadas = grafo.getCoresUsadas(arvoreGeradora);
+        
+        if(arvoreGeradora != null) {
+            for (Aresta a : arvoreGeradora) {
                 System.out.println("v"+a.getVerticeOrigem().getNome() + "--" +a.getValor()
                         + "--v" + a.getVerticeDestino().getNome() + "\t");
             }
             System.out.println();
-            System.out.println("Quantidade de cores = " + arvore.getCoresUsadas(arestasFinais).size());
+            System.out.println("Quantidade de cores = " + coresUsadas.size());
         } else
             System.out.println("Nao gera uma arvore");
         
         System.out.println("\nTempo Total: " + tempo + " milissegundos");
+        
     }
 }
--- src/VNS/Grafo.java
+++ src/VNS/Grafo.java
@@ -7,24 +7,45 @@ import java.util.Collections;
  *
  * @author sidemar
  */
-public class Arvore {
+public class Grafo {
     
-    public ArrayList<Aresta> gerarArvore(ArrayList<Aresta> arestas, int numVertices) {
+    private ArrayList<Aresta> arestas;
+    private int numVertices;
+
+    public Grafo(ArrayList<Aresta> arestas, int numVertices) {
+        this.arestas = arestas;
+        this.numVertices = numVertices;
+    }
+    
+    public ArrayList<Aresta> gerarArvore(ArrayList<Cor> cores) {
         ArrayList<Aresta> arvoreGeradora = new ArrayList<>();
+        ArrayList<Aresta> arestasCoresUsadas = new ArrayList();
         
-        for (Aresta a : arestas) {
+        Collections.sort(cores);
+        
+        for (Cor c : cores) {
+            for (Aresta a : this.arestas) {
+                if(a.getValor() == c.getCor()) {
+                    arestasCoresUsadas.add(0, a);
+                }
+            }
+        }
+        
+        Collections.reverse(arestasCoresUsadas);
+        
+        for (Aresta a : arestasCoresUsadas) {
             a.getVerticeOrigem().setUnion(UnionFind.makeSet(a.getVerticeOrigem()));
             a.getVerticeDestino().setUnion(UnionFind.makeSet(a.getVerticeDestino()));
         }
         
-        for(Aresta a : arestas) {
+        for(Aresta a : arestasCoresUsadas) {
             if(!UnionFind.areUnited(a.getVerticeOrigem().getUnion(), a.getVerticeDestino().getUnion())){
                 a.getVerticeOrigem().getUnion().union(a.getVerticeDestino().getUnion());
                 arvoreGeradora.add(0, a);
             }
         }
         
-        if((numVertices - 1) == arvoreGeradora.size()) // verifica se é uma árvore geradora
+        if((getNumVertices() - 1) == arvoreGeradora.size()) // verifica se é uma árvore geradora
             return arvoreGeradora;
         else
             return null;
@@ -57,4 +78,34 @@ public class Arvore {
         
         return cores;
     }
+
+    /**
+     * @return the arestas
+     */
+    public ArrayList<Aresta> getArestas() {
+        return arestas;
+    }
+
+    /**
+     * @param arestas the arestas to set
+     */
+    public void setArestas(ArrayList<Aresta> arestas) {
+        this.arestas = arestas;
+    }
+
+    /**
+     * @return the numVertices
+     */
+    public int getNumVertices() {
+        return numVertices;
+    }
+
+    /**
+     * @param numVertices the numVertices to set
+     */
+    public void setNumVertices(int numVertices) {
+        this.numVertices = numVertices;
+    }
+    
+    
 }
